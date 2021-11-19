5d1f896b7fa558cc3cbcf14a6eeaf1c2d3e1b4fa
Reece Gibbs <reece.gibbs@protonmail.com>
2021-11-19 15:54:41 +1300

    Installed node-scss and created a skeleton component for the SkillsMatrix component

---
+++ src/components/SkillsMatrix/SkillsMatrix.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './SkillsMatrix.module.scss';
+
+class SkillsMatrix extends React.Component {
+
+  render() {
+
+    return (
+      <h1 className={styles.skills_header}>This is our SkillsMatrix component</h1>
+    );
+  }
+}
+
+export default SkillsMatrix;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SkillsMatrix from './components/SkillsMatrix/SkillsMatrix';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <SkillsMatrix />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
